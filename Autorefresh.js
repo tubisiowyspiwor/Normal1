65f297b82d977fb810b797938cdaf28370ab3ebd
tubisiowyspiwor <tubisiowyspiwor@gmail.com>
2019-01-20 13:30:59 +0100

    Update Autorefresh.js

--- Autorefresh.js
+++ Autorefresh.js
@@ -33,5 +33,5 @@ $("#przycisk").click(function() {
                 if (hero.hp > 1){;
         window.location.reload();}
         }
-        }, Math.floor(Math.random() * 3600) + 4200);})
+        }, Math.floor(Math.random() * 360000) + 420000);})
 ();
