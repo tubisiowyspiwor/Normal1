1b32c0097d19f6a6181005f2746a888fdda8e8bc
tubisiowyspiwor <tubisiowyspiwor@gmail.com>
2019-01-27 18:52:04 +0100

    Update 3.js

--- 3.js
+++ 3.js
@@ -8,29 +8,6 @@ discordwebhook = (discordwebhook.length == 2 ? discordwebhook : [true, true]);
 for(var i in discordwebhook)
 	discordwebhook[i] = eval(discordwebhook[i]);
 
-{
-var menutoDW = $('<div/>')
-    .append($('<span tip="DiscordWebhook" style="color:#44A9CE">DW</span>'))
-    .append($('<input type="checkbox" tip="Powiadamiaj o e2 na Disordzie."/>')
-        .attr('checked',discordwebhook[0])
-        .change(function()
-            {
-                discordwebhook[0] = $(this).attr('checked');
-                data = new Date(); data.setTime(data.getTime()+3600000*24*31);
-                setCookie("discordwebhook", discordwebhook.join("|"), data);
-            }))
-	.append($('<input type="checkbox" tip="Powiadamiaj o herosach i tytanach na Discordzie."/>')
-        .attr('checked', discordwebhook[1])
-        .change(function()
-            {
-                discordwebhook[1] = $(this).attr('checked');
-                data = new Date(); data.setTime(data.getTime()+3600000*24*31);
-                setCookie("discordwebhook", discordwebhook.join("|"), data);
-            }));
-
-}
-
-
 (function(old) {
     newNpc = function(a) {
         old(a);
@@ -62,7 +39,7 @@ var menutoDW = $('<div/>')
                     },
                 });
               }
-         	if (a[i].wt>99 && discordwebhook[1] && a[i].lvl>36) {
+         	if (a[i].wt>99 && discordwebhook[1] && a[i].lvl>35) {
                 $.ajax({
                     url: "https://discordapp.com/api/webhooks/538471618704310283/tRIn33dDGJuRSc8q8QCIhDf4oYONQ64YRerfMraqqKEfKRokwdEOHLV16IdQOmrfoLlB",
                     type: "POST",
